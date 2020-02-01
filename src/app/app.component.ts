3eae38a245b0a8100ed7bae0b5f58a716d76d6b7
shubham <aggshubh95@gmail.com>
2020-02-01 22:42:07 +0530

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { AuthService } from './auth.service';
 })
 export class AppComponent {
   title = 'app';
-  constructor(private _authService: AuthService){}
+  constructor(public _authService: AuthService){}
 }
